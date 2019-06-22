4996d9baf9d698483e1d64c4b71fe1e2836e6e83
Taz <tammylimited@gmail.com>
2019-06-22 10:21:35 +0300

    added route to noteid, before setting params

--- js/routes.js
+++ js/routes.js
@@ -4,13 +4,22 @@ import emailApp from './apps/email/pages/email-app.cmp.js';
 import emailDetails from './apps/email/cmps/email-details.cmp.js';
 import emailList from './apps/email/cmps/email-list.cmp.js';
 import keepApp from './apps/keep/pages/keep-app.cmp.js';
+import notesListCmp from './apps/keep/cmps/notes-list.cmp.js';
+import noteItemCmp from './apps/keep/cmps/note-item.cmp.js'
 
 
 export default [
     { path: '/', component: homepageCmp },
-    { path: '/email', component: emailApp, children: [
-        { path: 'emails/:theEmailId', component: emailDetails},
-        { path: 'inbox', name: 'list', component: emailList}
-    ] },
+    {
+        path: '/email', component: emailApp, children: [
+            { path: 'emails/:theEmailId', component: emailDetails },
+            { path: 'inbox', name: 'list', component: emailList }
+        ]
+    },
     { path: '/keep', component: keepApp },
+    {
+        path: '/list', component: notesListCmp, childre: [
+            { path: '/list/:theItemId', component: noteItemCmp }
+        ]
+    },
 ]
