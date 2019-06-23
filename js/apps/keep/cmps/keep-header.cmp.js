6405a7d3892e47935dc161d4389fb8eceec96d7d
Ellah Toren <ellahtoren@gmail.com>
2019-06-23 11:56:03 +0300

    added sort and filter to keep

--- js/apps/keep/cmps/keep-header.cmp.js
+++ js/apps/keep/cmps/keep-header.cmp.js
@@ -2,19 +2,50 @@
 
 import mainNav from '../../../cmps/nav.cmp.js'
 import appLogo from '../../../cmps/logo.cmp.js'
+import keepSearch from '../cmps/keep-search.cmp.js';
+import keepFilter from '../cmps/keep-filter.cmp.js';
+import keepSort from '../cmps/keep-sort.cmp.js';
 
 export default {
     template: `
     <header class="keep-header">
         <app-logo></app-logo>
-        <h2>Search</h2>
-        <h2>Filter</h2>
-        <h2>Sort By</h2>
+        <div class="keep-searchAndFilter">
+            <keep-search @searchBy="emitSearchBy" @clearSearch="emitClear"></keep-search>
+            <keep-filter @filtered="emitFilterBy"></keep-filter>
+            <keep-sort @sorted="emitSortBy"></keep-sort>
+        </div>
         <main-nav></main-nav>
     </header>
     `,
+    methods: {
+        emitSearchBy(searchParam) {
+            console.log('got emited to search', searchParam)
+            // this.$emit('searchBy',searchParams);
+        },
+
+        emitFilterBy(filter)  {
+            console.log('got emited to filter',filter)
+            // this.$emit('filtered',filter);
+        },
+
+        emitSortBy(sorter)  {
+            console.log('got emited to sort', sorter)
+            // this.$emit('sorted',sorter);
+        },
+
+        emitClear() {
+            console.log('got emited to clear')
+            // this.$emit('clearSearch','');
+        }
+    },
+
     components: {
         mainNav,
-        appLogo
+        appLogo,
+        keepSearch,
+        keepFilter,
+        keepSort
     }
 }
+
---
+++ js/apps/keep/cmps/keep-sort.cmp.js
@@ -0,0 +1,38 @@
+'use strict'
+
+export default {
+    template: `
+        <section class="email-sort">
+            <h3>Sort <span class="fas fa-sort-amount-up-alt"></span></h3>
+            <div class="sorters">
+                <select v-model="sortBy.by">
+                    <option>Created</option>
+                    <option>Type</option>
+                    <option>Color</option>
+                </select>
+                <select v-model="sortBy.op">
+                    <option>+</option>
+                    <option>-</option>
+                </select>
+            </div>
+        </section>
+    `,
+
+    data() {
+        return {
+            sortBy: {by: 'Created', op: '-'}
+        }
+    },
+
+    watch: { 
+        'sortBy': {
+            handler: function(sorter) {
+                this.$emit('sorted', {by: sorter.by.toLowerCase(), op: sorter.op});
+           },
+           immediate: true,
+           deep: true
+         },
+   },
+
+
+}
