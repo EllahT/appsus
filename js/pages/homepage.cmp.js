4138860d0e9fe6d08b0885c868c2a1206f23e72b
Ellah Toren <ellahtoren@gmail.com>
2019-06-24 10:46:46 +0300

    fixed homepage design

--- js/pages/homepage.cmp.js
+++ js/pages/homepage.cmp.js
@@ -4,15 +4,17 @@ import mainNav from '../cmps/nav.cmp.js'
 export default  {
     template: `
     <section class="home">
-        <h1>Welcome to Strudel Appsus</h1>
-        <h4>by Tammy and Ellah </h4>
-        <main-nav></main-nav>
-        <p class="homepage-txt">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor dignissimos earum vitae temporibus omnis, sit sint minima blanditiis voluptatem ea accusantium, id cum, nam repudiandae rerum nulla voluptatum excepturi at?</p>
-        <span class="fas fa-angle-double-down arrows-nav" v-scroll-to="{
-            el: '#line-nav',
-            duration: 800,
-            easing: 'linear'}">
-        </span>
+        <header>
+            <h1>Welcome to Strudel Appsus</h1>
+            <h4>by Tammy and Ellah </h4>
+            <main-nav></main-nav>
+            <p class="homepage-txt">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor dignissimos earum vitae temporibus omnis, sit sint minima blanditiis voluptatem ea accusantium, id cum, nam repudiandae rerum nulla voluptatum excepturi at?</p>
+            <span class="fas fa-angle-double-down arrows-nav" v-scroll-to="{
+                el: '#line-nav',
+                duration: 800,
+                easing: 'linear'}">
+            </span>
+        </header>
         <div id="line-nav-bar">
             <nav id="line-nav">
                 <router-link to="/">Home</router-link> |
